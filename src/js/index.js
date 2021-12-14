43c914de383b62cfa5f429cdfb3d1dbfdca5145a
SEOYUN KIM <nado0903@gmail.com>
2021-12-14 17:33:50 +0900

    refactor: 코드 리팩토링

--- src/js/index.js
+++ src/js/index.js
@@ -36,7 +36,7 @@ function App() {
   };
 
   const updateTodoCount = () => {
-    const todoCount = $('#daily-todo-list').querySelectorAll('li').length; //querySelectorAll을 사용해서 ul태그 안의 모든 li태그를 가져옴
+    const todoCount = $('#todo-text-list').querySelectorAll('li').length; //querySelectorAll을 사용해서 ul태그 안의 모든 li태그를 가져옴
     $('.todo-count').innerText = `총 ${todoCount} 개`;
   };
 
@@ -61,29 +61,29 @@ function App() {
     </li>`;
       })
       .join('');
-    $('#daily-todo-list').innerHTML = template;
+    $('#todo-text-list').innerHTML = template;
     updateTodoCount();
   };
 
   const addTodo = () => {
-    if ($('#daily-todo').value === '') {
+    if ($('#todo-text').value === '') {
       alert('값을 입력해주세요');
       return;
     }
-    const dailytodo = $('#daily-todo').value;
-    this.todoItem[this.currentCategory].push({ text: dailytodo }); //push를 이용해 새로운 객체를 추가함
+    const todoText = $('#todo-text').value;
+    this.todoItem[this.currentCategory].push({ text: todoText }); //push를 이용해 새로운 객체를 추가함
     //상태가 변경됐을 때 localStorage에 바로 저장하고 읽어옴
     store.setLocalStorage(this.todoItem);
     //여러개의 item들이 렌더링 되어야 해서 map을 이용해 그 아이템별로 HTML 마크업을 만들 수 있게 함
     todoRender();
-    $('#daily-todo').value = '';
+    $('#todo-text').value = '';
   };
 
   const editTodo = e => {
     const todoId = e.target.closest('li').dataset.todoId;
     const $todoText = e.target.closest('li').querySelector('.todo-text');
     const renamedTodoText = prompt('할 일 수정', $todoText.innerText);
-    this.todoItem[todoId].text = renamedTodoText;
+    this.todoItem[this.currentCategory][todoId].text = renamedTodoText;
     store.setLocalStorage(this.todoItem);
     $todoText.innerText = renamedTodoText;
   };
@@ -92,14 +92,14 @@ function App() {
     const remove = confirm('삭제하시겠습니까?');
     if (remove) {
       const todoId = e.target.closest('li').dataset.todoId;
-      this.todoItem.splice(todoId, 1);
+      this.todoItem[this.currentCategory].splice(todoId, 1);
       store.setLocalStorage(this.todoItem);
       e.target.closest('li').remove(); // e.target.closest('li')는 li태그를 통으로 가져옴
       updateTodoCount();
     } else return;
   };
 
-  $('#daily-todo-list').addEventListener('click', e => {
+  $('#todo-text-list').addEventListener('click', e => {
     if (e.target.classList.contains('todo-remove-button')) {
       removeTodo(e);
     }
@@ -109,27 +109,29 @@ function App() {
   });
 
   //submit 이벤트가 발생했을 때 form태그 자동으로 전송되는 것을 막아줌
-  $('#daily-form').addEventListener('submit', e => {
+  $('#todo-form').addEventListener('submit', e => {
     e.preventDefault();
   });
 
   //확인 버튼 클릭 시
-  $('#daily-form-submit-button').addEventListener('click', addTodo);
+  $('#todo-form-submit-button').addEventListener('click', addTodo);
 
   //엔터 키 입력 시
-  $('#daily-todo').addEventListener('keypress', e => {
+  $('#todo-text').addEventListener('keypress', e => {
     if (e.key !== 'Enter') {
       return;
     }
     addTodo();
+  });
 
-    $('nav').addEventListener('click', e => {
-      const isCategoryButton =
-        e.target.classList.contains('todo-category-name');
-      if (isCategoryButton) {
-        const categoryName = e.target.dataset.categoryName;
-      }
-    });
+  $('nav').addEventListener('click', e => {
+    const isCategoryButton = e.target.classList.contains('todo-category-name');
+    if (isCategoryButton) {
+      const categoryName = e.target.dataset.categoryName;
+      this.currentCategory = categoryName;
+      $('#category-title').innerText = `${e.target.innerText} Plan`;
+      todoRender();
+    }
   });
 }
 
